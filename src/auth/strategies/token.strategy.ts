97bf4813556e66236306319df6c9d023e17f20a9
Eugen Halca <ehalca@gmail.com>
2022-05-06 11:08:22 +0300

    moved token validation

--- src/auth/strategies/token.strategy.ts
+++ src/auth/strategies/token.strategy.ts
@@ -3,17 +3,17 @@ import { PassportStrategy } from '@nestjs/passport';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { ConfigType } from 'src/config/config.module';
+import { AuthService } from '../services/auth.service';
 
 @Injectable()
 export class TokenStrategy extends PassportStrategy(Strategy, "custom") {
 
-    constructor(protected configService: ConfigService<ConfigType>) {
+    constructor(protected authService:AuthService) {
         super();
     }
 
     async validate(req) {
         const token = req.headers.authorization;
-        console.log('TBD: validate token, for now were validating against a hardcoded value', token);
-        return token === `Bearer ${this.configService.get('general.token', { infer: true })}`;
+        return this.authService.validateToken(token);
     }
 }
