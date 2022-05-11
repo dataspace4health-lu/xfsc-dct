7c711221250548085769391a781039b49b2cb3e5
Eugen Halca <ehalca@gmail.com>
2022-05-11 13:08:38 +0300

    review

--- src/inbox/controllers/inbox.controller.ts
+++ src/inbox/controllers/inbox.controller.ts
@@ -1,18 +1,18 @@
 import { Controller, Request, Post, UseGuards, Get } from '@nestjs/common';
 import { LocalAuthGuard } from 'src/auth/guards/local.guard';
-import { InboxService } from './inbox.service';
+import { TokenAuthGuard } from 'src/auth/guards/token.guard';
+import { InboxService } from '../services/inbox.service';
 
 @Controller('inbox')
+@UseGuards(TokenAuthGuard)
 export class InboxController {
-    constructor(private readonly inboxService: InboxService) {}
+    constructor(private readonly inboxService: InboxService) { }
 
-    @UseGuards(LocalAuthGuard)
     @Get()
     async getNotification(@Request() req): Promise<any> {
         return true;
     }
 
-    @UseGuards(LocalAuthGuard)
     @Post()
     async processNotification(@Request() req): Promise<any> {
         return true;
